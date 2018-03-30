a751cd8885e9e1f0eeb35f4eba263207abf5804b
dhirendrasingh <dvikramsingh93@gmail.com>
2018-03-30 21:05:43 +0530

    tesr

--- TestSelenium/src/Page/LoginPage.java
+++ TestSelenium/src/Page/LoginPage.java
@@ -12,8 +12,8 @@ public class LoginPage {
 	public static String signup=".//*[@id='gosuggest_inputSrc']";
 	
 	public static void login(){
-		Base.driver.findElement(By.xpath(signup)).sendKeys("test");
-		List<WebElement> l=Base.driver.findElements(By.xpath(".//*[@id='gi_class']//option"));
+		Base.driver.findElement(By.xpath("fff")).sendKeys("test");
+		List<WebElement> l=Base.driver.findElements(By.xpath("cdcd"));
 		for(WebElement l1:l){
 			System.out.println(l1.getText());
 		
